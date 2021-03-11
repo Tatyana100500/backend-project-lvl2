d0ee0abcb61a1700f7dd68cd7b6cd9f8acb327f7
Tatiana Chernishova <chernishovate@mail.ru>
2021-03-11 16:21:38 +0300

    step 1

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,3 +3,5 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 program.version('0.0.1', '-V, --version', 'output the version number');
 program.helpOption('-h, --help', 'output usage information');
+program.parse();
+console.log('Options: ', program.opts());
