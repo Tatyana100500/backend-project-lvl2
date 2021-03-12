dff2f6472f1fb5b08aaa28a6def5c435fb8d9974
Tatiana Chernishova <chernishovate@mail.ru>
2021-03-12 06:56:12 +0300

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,5 +3,14 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 program.version('0.0.1', '-V, --version', 'output the version number');
 program.helpOption('-h, --help', 'output usage information');
+program.option('-f, --format [type]', 'output format');
+program
+  .arguments('format')
+  .description('Compares two configuration files and shows a difference.', {
+    format: 'output format',
+  })
+  .action((format) => {
+    console.log('format:', format);
+  });
 program.parse();
 console.log('Options: ', program.opts());
