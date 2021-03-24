52d5534f221ccbcf7356bdf11992b6d004b779f8
Tatiana Chernishova <chernishovate@mail.ru>
2021-03-24 07:13:52 +0300

    step8

--- test/myDiff.test.js
+++ test/myDiff.test.js
@@ -1,25 +1,3 @@
-/*import { test, expect } from '@jest/globals';
-import fs from 'fs';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-import myDiff from './index.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-
-test('myDiff', () => {
-  const result = readFile('stylish');
-  expect(myDiff('testfile1.json', 'testfile2.json')).toEqual(result);
-  const result2 = readFile('stylish');
-  expect(myDiff('testfile1.json', 'testfile2.json', 'stylish')).toEqual(result2);
-  const result3 = readFile('stylish');
-  expect(myDiff('testfile1.yml', 'testfile2.yml')).toEqual(result3);
-});
-*/
 import { test, expect } from '@jest/globals';
 import * as url from 'url';
 import * as path from 'path';
